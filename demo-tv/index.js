c6a001651f9893f532c01000fec8cded92065bec
dario-vega <dario.vega@oracle.com>
2022-08-04 07:14:11 +0000

    test from OCI Code Editor

--- demo-tv/index.js
+++ demo-tv/index.js
@@ -1,3 +1,7 @@
+#
+# Copyright (c) 2022 Oracle, Inc.  All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
 const { ApolloServer, gql } = require('apollo-server');
 
 const fs = require('fs')
